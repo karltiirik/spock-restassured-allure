afe1497f7da76d73d19bd8314b6aca69d25d0074
Karl Tiirik <karl.tiirik@nortal.com>
2022-10-25 23:10:37 +0300

    Add surefireplugin conf to POM so tests would be picked up

--- src/test/groovy/HelloSpockTest.groovy
+++ src/test/groovy/HelloSpockTest.groovy
@@ -16,7 +16,7 @@
 
 import spock.lang.Specification
 
-class HelloSpockSpec extends Specification {
+class HelloSpockTest extends Specification {
   def "length of Spock's and his friends' names"() {
     expect:
     name.size() == length
