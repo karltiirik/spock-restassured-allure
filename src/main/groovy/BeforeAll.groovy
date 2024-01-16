4d8ea755a2b3978bde2a21192da1a96ba1a12232
Karl Tiirik <karltiirik@gmail.com>
2024-01-16 10:51:58 +0200

    fix(selenide): add missing configuration

--- src/main/groovy/BeforeAll.groovy
+++ src/main/groovy/BeforeAll.groovy
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.logevents.SelenideLogger
 import io.qameta.allure.restassured.AllureRestAssured
 import io.qameta.allure.selenide.AllureSelenide
@@ -15,6 +16,12 @@ class BeforeAll {
         // Selenide settings
         // Log selenide steps in allure
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide())
+        // Set browser size
+        Configuration.browserSize = "1920x1080"
+        // Run browser headless
+        Configuration.headless = true
+        // Set timeout to 10 sec
+        Configuration.timeout = 10000
     }
 
 }
